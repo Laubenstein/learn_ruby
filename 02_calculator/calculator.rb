1acf9286971293862354a897da031e55b335b21f
Dominic <dominic.laube@gmail.com>
2019-07-07 21:52:55 +0200

    asdf

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,35 @@
 #write your code here
+def add (a, b)
+    a + b 
+end
+
+def subtract (a, b)
+    a - b
+end
+
+def sum a
+    if a.empty?  
+    return 0
+    else 
+        a.reduce {|sum, n| sum += n}
+    end
+end
+
+def multiply (a, b)
+    a * b
+end
+
+def factorial a
+    if a == 0
+        1
+    elsif a == 1
+        1
+    else
+        number = 1
+        while a > 0
+            number = number * a 
+            a -= 1
+        end
+        return number
+    end
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
 #write your code here
+def echo string
+    string
+end
+
+def shout string
+    string.upcase
+end
+
+def repeat (string, times = 2) 
+    return_string = "#{string} " * times
+    return_string[0..-2]
+end
+
+def start_of_word (string, length) 
+    string[0..(length - 1)]
+end
+
+def first_word string
+    return_string = string.split(" ")
+    return_string[0]
+end
+
+def titleize string
+    return_string = string.split(" ").map{|word| word.capitalize}.join(" ")
+end
